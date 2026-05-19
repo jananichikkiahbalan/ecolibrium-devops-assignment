8cce83e82f3ff35420891301ba688d16881fdb82
jananichikkiahbalan <srijanani2830@gmail.com>
2026-05-19 14:33:48 +0530

    added eks-node-group

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -21,3 +21,34 @@ resource "aws_eks_cluster" "main" {
     Name = "${var.project_name}-${var.environment}-eks"
   }
 }
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.project_name}-${var.environment}-node-group"
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+
+  subnet_ids = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  capacity_type = "ON_DEMAND"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_policy
+  ]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-node-group"
+  }
+}
